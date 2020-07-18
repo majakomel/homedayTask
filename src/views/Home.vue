8cb0e13b5797f541046616b4d930014594dbfa47
Maja Komel <maja.komel@gmail.com>
2020-07-18 10:52:17 +0200

    add home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,74 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main>
+    <form @submit.prevent="onSubmit" class="form">
+      <label for="username" class="form__label">
+        Type in the GitHub username you want to search for:
+      </label>
+      <div class="form__inputWrapper">
+        <input
+          type="text"
+          id="username"
+          class="form__input"
+          name="username"
+          autocomplete="off"
+          placeholder="username"
+          v-model="username"
+          v-bind:class="{ 'form__input--errored': error }"
+        />
+        <div v-if="error" class="form__errorMsg">{{ error }}</div>
+      </div>
+
+      <button type="submit">Search</button>
+    </form>
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      username: "",
+      error: null
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.error = null;
+      if (!this.username) {
+        this.error = "You have to type in the username.";
+        return;
+      }
+
+      this.$router.push({
+        name: "results",
+        params: { username: this.username }
+      });
+    }
   }
 };
 </script>
+
+<style lang="scss">
+.form {
+  &__inputWrapper {
+    position: relative;
+    margin-bottom: 20px;
+  }
+  &__input {
+    width: 200px;
+    &--errored {
+      border: 1px solid red;
+      border-radius: 4px;
+    }
+  }
+  &__label {
+    display: block;
+    margin-bottom: 10px;
+  }
+  &__errorMsg {
+    position: absolute;
+    font-size: 12px;
+    color: red;
+  }
+}
+</style>
